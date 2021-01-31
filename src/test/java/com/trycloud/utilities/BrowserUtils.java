f23667a24cec42cf76911fc5b19d3ec290017008
IlgarA <ilqar.nureddinoglu@gmail.com>
2021-01-30 21:08:46 -0500

    Utility created

--- src/test/java/com/trycloud/utilities/BrowserUtils.java
+++ src/test/java/com/trycloud/utilities/BrowserUtils.java
@@ -1,4 +1,22 @@
 package com.trycloud.utilities;
-
+/*
+We will create utility methods for repeated steps that can be used
+in browsers, and store in this class.
+ */
 public class BrowserUtils {
+
+    //create method name: wait
+    //converting milliseconds to seconds
+    //handle checked exception
+    public static void sleep(int second){
+
+        second *= 1000;
+
+        try {
+            Thread.sleep(second);
+        } catch (InterruptedException e) {
+            System.out.println("Soething happend in the method");
+        }
+    }
+
 }
